a1b66479c2baafd66380e6e1c43ec959a1a92a71
serfrae <a@serfrae.com>
2024-07-17 21:23:35 +0100

    complete blink

--- blink/src/main.rs
+++ blink/src/main.rs
@@ -1,6 +1,7 @@
 use {
+	anyhow::{anyhow, Result},
 	axum::{
-		extract::{Json, Query},
+		extract::{Json, Query, State},
 		http::{
 			header::{ACCEPT_ENCODING, AUTHORIZATION, CONTENT_ENCODING, CONTENT_TYPE},
 			Method, StatusCode,
@@ -11,6 +12,7 @@ use {
 	},
 	base64::{engine::general_purpose::STANDARD, Engine},
 	bincode::serialize,
+	clap::Parser,
 	serde::{Deserialize, Serialize},
 	serde_json::{json, Value},
 	solana_client::rpc_client::RpcClient,
@@ -19,13 +21,55 @@ use {
 		transaction::Transaction,
 	},
 	std::str::FromStr,
+	std::sync::Arc,
 	stuk_wl::instructions,
 	tokio::net::TcpListener,
 	tower_http::cors::{Any, CorsLayer},
 };
 
+struct AppState {
+	mint: Pubkey,
+	rpc_client: RpcClient,
+}
+
+#[derive(Parser, Debug)]
+struct Cli {
+	mint: Pubkey,
+	url: Option<String>,
+	config: Option<String>,
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+	let args = Cli::parse();
+
+	let mint = args.mint;
+	let solana_config_file = match args.config {
+		Some(path) => solana_cli_config::Config::load(&path).unwrap_or_default(),
+		None => {
+			if let Some(ref config) = *solana_cli_config::CONFIG_FILE {
+				solana_cli_config::Config::load(config).unwrap_or_default()
+			} else {
+				solana_cli_config::Config::default()
+			}
+		}
+	};
+
+	let url = match args.url {
+		Some(id) => match id.as_str() {
+			"t" | "testnet" => "https://api.testnet.solana.com".to_string(),
+			"d" | "devnet" => "https://api.devnet.solana.com".to_string(),
+			"m" | "mainnet" => "https://api.mainnet-beta.solana.com".to_string(),
+			"l" | "local" => "http://localhost:8899".to_string(),
+			_ => id,
+		},
+		None => solana_config_file.json_rpc_url,
+	};
+
+	let rpc_client = RpcClient::new_with_commitment(url, CommitmentConfig::confirmed());
+
+	let state = Arc::new(AppState { mint, rpc_client });
+
 	let cors = CorsLayer::new()
 		.allow_methods([Method::GET, Method::POST, Method::OPTIONS])
 		.allow_headers([
@@ -42,10 +86,13 @@ async fn main() {
 		.route("/api/actions/buy-token", post(buy_post_request_handler))
 		.route("/api/actions/register", get(reg_get_request_handler))
 		.route("/api/actions/register", post(reg_post_request_handler))
-		.layer(cors);
+		.layer(cors)
+		.with_state(state);
 
 	let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-	axum::serve(listener, app).await.unwrap();
+	axum::serve(listener, app)
+		.await
+		.map_err(|e| anyhow!("Could not start webserver: {}", e))
 }
 
 async fn get_request_actions_json() -> impl IntoResponse {
@@ -154,6 +201,7 @@ struct PostResponse {
 }
 
 async fn buy_post_request_handler(
+	State(state): State<Arc<AppState>>,
 	Query(params): Query<QueryParams>,
 	Json(payload): Json<PostRequest>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<Value>)> {
@@ -163,27 +211,51 @@ async fn buy_post_request_handler(
 			Json(json!({"error": "Invalid 'account' provided"})),
 		)
 	})?;
-	let to_pubkey = Keypair::new().pubkey();
-	let rpc_client = RpcClient::new_with_commitment(
-		"https://api.devnet.solana.com".to_string(),
-		CommitmentConfig::confirmed(),
-	);
 
-	let latest_blockhash = rpc_client.get_latest_blockhash().map_err(|err| {
+	let latest_blockhash = state.rpc_client.get_latest_blockhash().map_err(|err| {
 		(
 			StatusCode::INTERNAL_SERVER_ERROR,
 			Json(json!({"error": format!("Failed to get latest blockhash: {}", err)})),
 		)
 	})?;
 
+	let (whitelist, _) = stuk_wl::get_whitelist_address(&state.mint);
+	let mint_account = state.rpc_client.get_account(&state.mint).map_err(|err| {
+		(
+			StatusCode::INTERNAL_SERVER_ERROR,
+			Json(json!({"error": format!("Failed to get mint account: {}", err)})),
+		)
+	})?;
+	let token_program = mint_account.owner;
+	let vault = spl_associated_token_account::get_associated_token_address_with_program_id(
+		&whitelist,
+		&state.mint,
+		&token_program,
+	);
+	let (ticket, _) = stuk_wl::get_user_ticket_address(&account, &whitelist);
+	let ticket_token_account =
+		spl_associated_token_account::get_associated_token_address_with_program_id(
+			&ticket,
+			&state.mint,
+			&token_program,
+		);
+	let user_token_account =
+		spl_associated_token_account::get_associated_token_address_with_program_id(
+			&account,
+			&state.mint,
+			&token_program,
+		);
+
 	let instruction = instructions::buy_tokens(
-		&Pubkey::new_unique(),
-		&Pubkey::new_unique(),
-		&Pubkey::new_unique(),
-		&Pubkey::new_unique(),
-		&Pubkey::new_unique(),
-		&Pubkey::new_unique(),
+		&whitelist,
+		&vault,
+		&state.mint,
+		&account,
+		&ticket,
+		&ticket_token_account,
+		&user_token_account,
 		params.amount as u64,
+		&token_program,
 	)
 	.map_err(|err| {
 		(
@@ -203,11 +275,12 @@ async fn buy_post_request_handler(
 
 	Ok(Json(PostResponse {
 		transaction: STANDARD.encode(serialized_transaction),
-		message: format!("Send {} SOL to {}", params.amount, to_pubkey),
+		message: format!("Buying {} tokens", params.amount),
 	}))
 }
 
 async fn reg_post_request_handler(
+	State(state): State<Arc<AppState>>,
 	Query(params): Query<QueryParams>,
 	Json(payload): Json<PostRequest>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<Value>)> {
@@ -230,16 +303,10 @@ async fn reg_post_request_handler(
 		)
 	})?;
 
-	let instruction = instructions::register(
-		&Pubkey::new_unique(),
-		&Pubkey::new_unique(),
-		&Pubkey::new_unique(),
-		&Pubkey::new_unique(),
-		&Pubkey::new_unique(),
-		&Pubkey::new_unique(),
-		params.amount as u64,
-	)
-	.map_err(|err| {
+	let (whitelist, _) = stuk_wl::get_whitelist_address(&state.mint);
+	let (ticket, _) = stuk_wl::get_user_ticket_address(&account, &whitelist);
+
+	let instruction = instructions::register(&whitelist, &account, &ticket).map_err(|err| {
 		(
 			StatusCode::INTERNAL_SERVER_ERROR,
 			Json(json!({"error": format!("Could not create `Register` instruction: {}", err)})),
